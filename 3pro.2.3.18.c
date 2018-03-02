b354363be0fc58b27bd1a999c5b23ad2b0413032
saran7237 <36067371+saran7237@users.noreply.github.com>
2018-03-02 13:14:41 +0530

    Create 3pro.2.3.18.c

---
+++ 3pro.2.3.18.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+void swap(int *x,int *y)
+{
+int temp=*x;
+*x=*y;
+*y=temp;
+}
+int main()
+{
+int x,y;
+printf("enter x:");
+scanf("%d",&x);
+printf("enter y:");
+scanf("%d",&y);
+swap(&x,&y);
+printf("after swapping x=%d and y=%d",x,y);
+return 0;
+getch();
+}
